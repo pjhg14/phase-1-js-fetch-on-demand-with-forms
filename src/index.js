6dce604eca6cbfe98b892d305ad9cdf9b7c39cd6
pj <pjhg14@gmail.com>
2021-03-23 18:19:11 -0400

    commit tree 8642eb314a5667193db52f4263881deb02cc0e84

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
+    const inputForm = document.querySelector("form")
+    const title = document.querySelector("section#movieDetails h4");
+    const summary = document.querySelector("section#movieDetails p");
+    
+    inputForm.addEventListener("submit", event => {
+        event.preventDefault()
+        const input = document.querySelector('input#searchByID');
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+            .then(res => res.json())
+            .then(movie => {
+                title.innerText = movie.title
+                summary.innerText = movie.summary
+            })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
